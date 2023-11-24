d4bf1c49ae75a69815cc8ed262d45d5befa51aea
femm34 <femm15.mm@gmail.com>
2023-11-23 20:59:19 -0600

    "api terminada"

--- src/app.js
+++ src/app.js
@@ -6,8 +6,16 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+const routerApi = require("./routes");
+
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("ahs");
+});
+
+routerApi(app);
 
 app.listen(port, () => {
   console.log(`App running at http://localhost:${port}`);
