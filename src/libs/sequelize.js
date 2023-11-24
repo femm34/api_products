c1cf130df65101fe92b397f81f5a9a33f7924847
femm34 <femm15.mm@gmail.com>
2023-11-23 23:06:14 -0600

    api products is done

--- src/libs/sequelize.js
+++ src/libs/sequelize.js
@@ -2,10 +2,15 @@ const { Sequelize } = require("sequelize");
 const config = require("../config/config.js");
 const setupModels = require("./../database/models");
 
-const sequelize = new Sequelize("hola", "root", "root", {
-  host: "127.0.0.1",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME || "products",
+  process.env.DB_USER || "root",
+  process.env.DB_PASSWORD || "",
+  {
+    host: process.env.DB_HOST || "localhost",
+    dialect: "mysql",
+  }
+);
 
 sequelize
   .sync()
